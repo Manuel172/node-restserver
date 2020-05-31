65e898d8cc875be5512a25c83df549353948ce81
Manuel172 <manuelsalasc@hotmail.com>
2020-05-31 18:11:15 -0400

    Variables de entorno MONGO - Atlas

--- server/config/config.js
+++ server/config/config.js
@@ -9,10 +9,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //=================== Base de Datos =======================
 let urlDB;
 
-if (process.env.NODE_ENV = 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe_dj';
 } else {
-    urlDB = 'mongodb+srv://administradorcafe:caffeCaribe@cluster0-hsccs.mongodb.net/cafe_dj';
+    // el string de conección lo entrega mongo en atlas como el siguiente ejemplo
+    //"mongodb+srv://<usuario>:<clave>@cluster0-hsccs.mongodb.net/<Base de Datos>"
+    // este se crea en heroku  como variable de entorno para que no queden publicas las credenciales en GitHub
+    urlDB = process.env.DireccionMongo;
 }
-urlDB = 'mongodb+srv://administradorcafe:caffeCaribe@cluster0-hsccs.mongodb.net/cafe_dj';
 process.env.urlDB = urlDB;
